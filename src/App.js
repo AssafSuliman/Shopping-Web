f547b8972d7c330184ac5e93761e1c9bac27b7ba
Assaf <asafaaaad2@gmail.com>
2022-07-05 14:11:18 +0300

    added ProductPage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import ProductPage from './ProductPage.js'
 
 function App() {
   return (
     <div >
-      
+      <ProductPage></ProductPage>
     </div>
   );
 }
